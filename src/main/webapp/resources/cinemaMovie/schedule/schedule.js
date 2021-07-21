69fb7385507965a1683b0f7a7a4d15daa1190c6a
HEOYONGSEUNG <gjdydtmd1234@gmail.com>
2021-07-21 16:45:16 +0900

    영화상영시간표 js

--- src/main/webapp/resources/cinemaMovie/schedule/schedule.js
+++ src/main/webapp/resources/cinemaMovie/schedule/schedule.js
@@ -6,7 +6,7 @@ const weekDay = document.querySelector('.weekDay')
 const dayList = document.querySelector('.dayList')
 
 var nowDate = new Date();
-var todayDate = new Date(2021,6);
+var todayDate = new Date();
 const year = todayDate.getFullYear();
 const month = todayDate.getMonth() + 1;
 const date =  todayDate.getDate();
@@ -22,6 +22,30 @@ const userDayName = days[todayDate.getDay()];
 const DateAll = userYear + '년' + userMonth + '월' + userDate + '일' + '(' + userDayName + ')'
 allDate.innerText = DateAll
 
+let newMonth = month < 10 ? '0' + month : month  
+		let newDate = todayDate.getDate() < 10 ? '0' + todayDate.getDate() : todayDate.getDate()
+		const DateAllJson = String(year) + newMonth + String(newDate)
+		console.log(DateAllJson)
+		let tosDate = {
+		date : DateAllJson
+	}
+		console.log(tosDate.date)
+		let tosDateJson = JSON.stringify(tosDate)
+		
+		const url = "schedule/list/" + tosDateJson+"/" 
+		const opt = {
+				method : 'GET'
+		}
+		
+		window.onload = function(event) {
+			fetch(url,opt)
+			.then(resp => {
+				return resp.json()
+			})
+			.then(value1 => {
+				console.log(value1)
+			})
+		}
 //일자 누르면 변동
 for(let i = userDate; i < userDate + 12; i++) {
 	todayDate.setDate(i) 
@@ -33,30 +57,18 @@ for(let i = userDate; i < userDate + 12; i++) {
 		dayList.appendChild(dateDiv)
 	}
 	const DateAll2 = year + '년' + month + '월' + todayDate.getDate() + '일' + "(" + days[todayDate.getDay()] + ")"
-	let newMonth = month < 10 ? '0' + month : month  
-	const DateAllJson = String(year) + newMonth + String(todayDate.getDate())
 	
 	dateDiv.onclick = function(event) {
 		console.log(dateDiv.innerText)
 		console.log(i)
 		allDate.innerText = DateAll2
-		console.log(DateAllJson)
-		let tosDate = {
-			date : DateAllJson
-		}
-		console.log(tosDate.date)
-		let tosDateJson = JSON.stringify(tosDate)
-		console.log()
-		const url = "schedule/list/" + tosDateJson+"/" 
-		const opt = {
-			method : 'GET'
-		}
+		
 		fetch(url,opt)
 		.then(resp => {
 			return resp.json()
 		})
-		.then(json => {
-			console.log(json)
+		.then(value2 => {
+			console.log(value2)
 		})
 
 	}
